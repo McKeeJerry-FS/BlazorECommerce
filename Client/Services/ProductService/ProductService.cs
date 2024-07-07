58bde4453961ffbddec3343e77437eff7dac4f99
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-07-07 15:17:25 -0400

    Added productdetails to the project and modified the css

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,20 @@ namespace BlazorECommerse.Client.Services.ProductService
         }
         public List<Product>? Products { get; set; }
 
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            if (result is not null)
+            {
+                return result;
+            }
+            else
+            {
+               return new ServiceResponse<Product> { Success = false, Message = "Product not found." };
+            }
+            
+        }
+
         public async Task GetProducts()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
--- Server/Services/ProductServices/ProductService.cs
+++ Server/Services/ProductServices/ProductService.cs
@@ -12,6 +12,25 @@ namespace BlazorECommerse.Server.Services.ProductServices
             _context = context;
         }
 
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Product>();
+            var product = await _context.Products.FindAsync(productId);
+            if (product is null)
+            {
+                response.Success = false;
+                response.Message = "Product not found";
+            }
+            else
+            {
+                response.Data = product;
+            }
+
+            return response;
+            
+
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
         {
             var response = new ServiceResponse<List<Product>>
