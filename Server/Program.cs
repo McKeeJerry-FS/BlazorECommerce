da19f1d0f251e9490d224f511177e7d5f6b0ef19
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-07-06 22:43:22 -0400

    added migrations, created and updated databases; Seeded new data into the database; data now received from BlazorEcommerceDb

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,16 @@
 global using BlazorECommerse.Shared;
 global using Microsoft.EntityFrameworkCore;
+global using BlazorECommerse.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
