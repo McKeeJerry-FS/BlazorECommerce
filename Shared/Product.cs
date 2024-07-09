84e2d13afce34e5140ac5a4811395cee94efe91e
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-07-09 11:21:00 -0400

    Added product types and productVariant models to the program

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -13,8 +13,7 @@ namespace BlazorECommerse.Shared
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
 
         // Category navigation property
         public Category? Category { get; set; }
